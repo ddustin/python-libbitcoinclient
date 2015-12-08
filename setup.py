4b06d478dae55188d7975013844d43f585aea453
Chris Pacia <ctpacia@gmail.com>
2015-12-08 10:15:58 -0500

    Refactor to python-libbitcoinclient

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='python-obelisk',
+    name='python-libbitcoinclient',
     version="0.2.0",
     install_requires=['twisted', 'ecdsa', 'pyzmq'],
     packages=['obelisk'],
     maintainer='Dionysis Zindros',
     maintainer_email='dionyziz@gmail.com',
     zip_safe=False,
-    description="Python native client for the obelisk blockchain server.",
+    description="Python native client for the libbitcoin blockchain server.",
     long_description=open('README.txt').read(),
     license='GNU Affero General Public License',
-    keywords='bitcoin blockchain obelisk obeliskoflight query transaction federated',
-    url='https://github.com/darkwallet/python-obelisk'
+    keywords='bitcoin blockchain libbitcoin query transaction federated',
+    url='https://github.com/OpenBazaar/python-libbitcoinclient'
 )
